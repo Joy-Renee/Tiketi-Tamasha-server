984b5241654d5a2a2e89d5d859a24d6bc83a55bd
Joy-Renee <152525597+Joy-Renee@users.noreply.github.com>
2024-08-12 09:24:12 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS, cross_origin
 from flask_migrate import Migrate
 from flask_swagger_ui import get_swaggerui_blueprint
 from datetime import datetime
-from models import db, Customer, Ticket, Booking, Organizer, Venue, Event, Order, Payment
+from .models import db, Customer, Ticket, Booking, Organizer, Venue, Event, Order, Payment
 import os
 from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
